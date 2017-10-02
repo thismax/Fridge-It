47b94916c0e6954ac569863abc883bdda1d0a52e
Daniel Chong <danchong625@gmail.com>
2017-10-02 10:52:52 -0700

    [Document] Documentation and syntax fix

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, createStore, compose } from 'redux';
 import createHistory from 'history/createBrowserHistory';
 import { Router } from 'react-router-dom';
-import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
 import { createLogger as logger } from 'redux-logger';
 import promise from 'redux-promise-middleware';
@@ -14,9 +14,11 @@ import App from './component/app.jsx';
 import HomeView from './component/Home/homeView.jsx';
 import LandingPage from './component/Landing_Page/landingPage.jsx';
 
+// Initialize history for router and middleware for redux
 const history = createHistory();
 const middleware = applyMiddleware(promise(), thunk, logger(), routerMiddleware(history));
 
+// Initialize redux state with (reducers, middleware)
 const store = createStore(FridgeApp, middleware);
 
 DOMReact.render(
